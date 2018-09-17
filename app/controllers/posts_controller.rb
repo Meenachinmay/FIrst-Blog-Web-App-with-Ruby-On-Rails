b03038c5e34445e33cf40829a227377d7a1d941f
meenachinmay <chinmayanand896@gmail.com>
2018-09-17 17:13:22 +0530

    further changes added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
     # {index} action in posts {controller}
     def index
+        @posts = Post.all.order("created_at DESC")
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    def show 
+        @post = Post.find(params[:id])
+    end
+
+    private 
+    def post_params
+        params.require(:post).permit(:title, :content)
     end
 end
